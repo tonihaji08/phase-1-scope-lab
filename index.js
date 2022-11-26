8eb841a64e5e58a01ef9fa2e08114c7cc485cf41
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-27 00:28:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+ function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+const leastFavoriteCustomer = "lydya";
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "mercy";
+    return leastFavoriteCustomer;
+}
